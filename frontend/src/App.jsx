1eb8046a6accba4542aec749a5ccee5984427bee
b-ezawit <bezawitbiazine@gmail.com>
2026-01-19 09:56:59 +0300

    feat: add EventsTestimonials route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 
-
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Gallery from "./pages/Gallery";
+import EventsTestimonials from "./pages/EventsTestimonials";
 
 const App = () => {
   return (
@@ -13,7 +13,13 @@ const App = () => {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
-          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          
+          {/* Matches your Navbar link: "/Events&Testimonials" */}
+          <Route path="/EventsTestimonials" element={<EventsTestimonials />} />
+          
+          {/* Added this so your 'Book a table' button works */}
+          <Route path="/reservation" element={<div>Reservation Page</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
