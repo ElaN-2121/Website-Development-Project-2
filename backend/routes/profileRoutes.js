73816fa6a560d0c2f52067668cbc174ddef0024e
imanbelete <iman.ugr-8533-16@aau.edu.et>
2026-01-30 21:53:22 +0800

    Correct profile route import to prevent runtime error

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,8 +1,10 @@
-const { adminMiddleware } = require("../middleware/adminMiddleware");
-const { getProfile, updateProfile } = require("../controllers/profileController");
+const adminMiddleware = require("../middleware/adminMiddleware");
+const {
+  getProfile,
+  updateProfile,
+} = require("../controllers/profileController");
 
 const profileRoutes = async (req, res) => {
-
   if (req.url === "/api/profile" && req.method === "GET") {
     if (!adminMiddleware(req, res)) return true;
     await getProfile(req, res);
